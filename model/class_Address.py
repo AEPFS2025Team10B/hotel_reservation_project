7bca61a017d2fb6e6f050dee10d455c98f0215d7
Jeremy Nathan <jeremyevrjr.nathan@gmail.com>
2025-05-06 20:54:37 +0200

    I made the attributes of the class Address (private) and added the getters and setters

--- model/class_Address.py
+++ model/class_Address.py
@@ -1,7 +1,60 @@
 class Address:
     def __init__(self, address_id:int, street:str, city:str, state:str, zip_code:str):
-        self.address_id = address_id
-        self.street = street
-        self.city = city
-        self.state = state
-        self.zip_code = zip_code
+        self.__address_id = address_id
+        self.__street = street
+        self.__city = city
+        self.__state = state
+        self.__zip_code = zip_code
+
+    @property
+    def address_id(self):
+        return self.__address_id
+
+    @property
+    def street(self):
+        return self.__street
+
+    @street.setter
+    def street(self, street):
+        if not street:
+            raise ValueError("street name and number is required")
+        if not isinstance(street, str):
+            raise TypeError("street must be an string")
+        self.__street = street
+
+    @property
+    def city(self):
+        return self.__city
+
+    @city.setter
+    def city(self, city):
+        if not city:
+            raise ValueError("city name is required")
+        if not isinstance(city, str):
+            raise TypeError("city must be an string")
+        self.__city = city
+
+    @property
+    def state(self):
+        return self.__state
+
+    @state.setter
+    def state(self, state):
+        if not state:
+            raise ValueError("state name is required")
+        if not isinstance(state, str):
+            raise TypeError("state must be an string")
+        self.__state = state
+
+    @property
+    def zip_code(self):
+        return self.__zip_code
+
+    @zip_code.setter
+    def zip_code(self, zip_code):
+        if not zip_code:
+            raise ValueError("zip code is required")
+        if not isinstance(zip_code, str):
+            raise TypeError("zip code must be an string")
+        self.__zip_code = zip_code
+
