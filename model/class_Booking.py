a1a956239d48912e13949a8682e4bb1a3da5cdd8
Jeremy Nathan <jeremyevrjr.nathan@gmail.com>
2025-05-06 21:01:12 +0200

    I changed made the attributes of class booking (private) and added getters and setters but incomplete

--- model/class_Booking.py
+++ model/class_Booking.py
@@ -1,9 +1,52 @@
 from datetime import date
 class Booking:
     def __init__(self, booking_id:int, check_in_date:date, check_out_date:date, number_of_guests:int):
-        self.booking_id = booking_id
-        self.check_in_date = check_in_date
-        self.check_out_date = check_out_date
-        self.number_of_guests = number_of_guests
-        self.is_cancelled = False
-        self.total_price = 0
+        self.__booking_id = booking_id
+        self.__check_in_date = check_in_date
+        self.__check_out_date = check_out_date
+        self.__number_of_guests = number_of_guests
+        self.__is_cancelled = False
+        self.__total_price = 0
+
+    @property
+    def booking_id(self):
+        return self.__booking_id
+
+    @property
+    def check_in_date(self):
+        return self.__check_in_date
+
+    @check_in_date.setter
+    def check_in_date(self, check_in_date):
+        pass
+
+    @property
+    def check_out_date(self):
+        return self.__check_out_date
+
+    @check_out_date.setter
+    def check_out_date(self, check_out_date):
+        pass
+
+    @property
+    def number_of_guests(self):
+        return self.__number_of_guests
+
+    @number_of_guests.setter
+    def number_of_guests(self, number_of_guests):
+        pass
+
+    @property
+    def is_cancelled(self):
+        return self.__is_cancelled
+
+    @is_cancelled.setter
+    def is_cancelled(self, is_cancelled):
+        pass
+
+    @property
+    def total_price(self):
+        return self.__total_price
+    @total_price.setter
+    def total_price(self, total_price):
+        pass
