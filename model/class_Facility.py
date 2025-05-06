3c49cedd3d2242829183283bbe9f9ff4647a02df
Jeremy Nathan <jeremyevrjr.nathan@gmail.com>
2025-05-06 21:06:33 +0200

    I made the attributes of the class facility (private) and added getter and setter

--- model/class_Facility.py
+++ model/class_Facility.py
@@ -1,4 +1,19 @@
 class Facility:
     def __init__(self, facility_id:int, name:str):
-        self.facility_id = facility_id
-        self.name = name
+        self.__facility_id = facility_id
+        self.__name = name
+
+    @property
+    def facility_id(self):
+        return self.__facility_id
+
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("facility name is required")
+        if not isinstance(name, str):
+            raise TypeError("facility name must be an string")
+        self.__name = name
