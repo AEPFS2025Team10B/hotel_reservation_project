6ceb82abf6d5586a14f2cf118f47bd903460a605
Jeremy Nathan <jeremyevrjr.nathan@gmail.com>
2025-05-06 21:28:18 +0200

    I made the attributes of the class Invoice (private) and added getters, setters. Not finished

--- model/class_Invoice.py
+++ model/class_Invoice.py
@@ -1,6 +1,26 @@
 from datetime import date
 class Invoice:
     def __init__(self, invoice_id:int, issue_date:date, total_amount:float):
-        self.invoice_id = invoice_id
-        self.issue_date = issue_date
-        self.total_amount = total_amount
+        self.__invoice_id = invoice_id
+        self.__issue_date = issue_date
+        self.__total_amount = total_amount
+
+    @property
+    def invoice_id(self):
+        return self.__invoice_id
+
+    @property
+    def issue_date(self):
+        return self.__issue_date
+
+    @issue_date.setter
+    def issue_date(self, issue_date):
+        pass
+
+    @property
+    def total_amount(self):
+        return self.__total_amount
+
+    @total_amount.setter
+    def total_amount(self, total_amount):
+        pass
