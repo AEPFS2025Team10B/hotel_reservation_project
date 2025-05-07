ace379829b95f666922b1fc4a4d5d430311cb5cb
retolutz <reto.lutz4@me.com>
2025-05-07 11:44:31 +0200

    Guest Data Access erstellt

---
+++ data_access/guest_data_access.py
@@ -0,0 +1,80 @@
+import model.class_Guest
+from data_access.base_data_access import BaseDataAccess
+
+
+class GuestDataAccess(BaseDataAccess):
+    def __init__(self, db_path: str = None):
+        super().__init__(db_path)
+
+    def create_new_guest(
+            self,
+            first_name: str,
+            last_name: str,
+            email: str
+    ) -> model.class_Guest.Guest:
+        if first_name is None:
+            raise ValueError("First name is required")
+        if last_name is None:
+            raise ValueError("Last name is required")
+        if email is None:
+            raise ValueError("Email is required")
+
+        sql = """
+        INSERT INTO Guest (FirstName, LastName, Email)
+        VALUES (?, ?, ?)
+        """
+        params = (first_name, last_name, email)
+
+        last_row_id, row_count = self.execute(sql, params)
+
+        return model.class_Guest.Guest(
+            guest_id=last_row_id,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+
+    def read_guest_by_id(self, guest_id: int) -> model.class_Guest.Guest | None:
+        if guest_id is None:
+            raise ValueError("Guest ID is required")
+
+        sql = """
+        SELECT GuestId, FirstName, LastName, Email
+        FROM Guest
+        WHERE GuestId = ?
+        """
+        params = (guest_id,)
+        result = self.fetchone(sql, params)
+
+        if result:
+            guest_id, first_name, last_name, email = result
+            return model.class_Guest.Guest(
+                guest_id=guest_id,
+                first_name=first_name,
+                last_name=last_name,
+                email=email
+            )
+        else:
+            return None
+
+    def read_guests_by_last_name(self, last_name: str) -> list[model.class_Guest.Guest]:
+        if last_name is None:
+            raise ValueError("Last name is required")
+
+        sql = """
+        SELECT GuestId, FirstName, LastName, Email
+        FROM Guest
+        WHERE LastName = ?
+        """
+        params = (last_name,)
+        rows = self.fetchall(sql, params)
+
+        return [
+            model.class_Guest.Guest(
+                guest_id=row[0],
+                first_name=row[1],
+                last_name=row[2],
+                email=row[3]
+            )
+            for row in rows
+        ]
