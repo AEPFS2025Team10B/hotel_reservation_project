3c0d4c91e4455013c5ab005a87263b1ac53f17f0
Jeremy Nathan <jeremyevrjr.nathan@gmail.com>
2025-05-12 13:35:52 +0200

    Did some changes in the model class hotel

--- model/hotel.py
+++ model/hotel.py
@@ -8,8 +8,8 @@ class Hotel:
         self.__name = name
         self.__stars = stars
         self.__address = Address
-        self.__room = list[Room] = []
-        self.__facility = list[Facility] = []
+        self.__room  = []
+        self.__facility = []
 
 def __repr__(self):
     return f"Hotel(id={self.__hotel_id!r}, name={self.__name!r})"
@@ -49,33 +49,31 @@ def __repr__(self):
         return self.__address
 
     @address.setter
-    def address(self, address):
-        if not isinstance(address, Address):
-            raise TypeError("address is required")
-        self.__address = address
+    def add_address(self, address: Address):
+        self.__address.append(address)
 
     @property
     def room(self):
         return self.__rooms
 
-    @rooms.setter
-    def room(self, room):
-        if not isinstance(room, Room):
-            raise TypeError("room must be a Room")
-        self.__room = room
+    def add_room(self, room: Room):
+        self.__room.append(room)
 
     @property
     def facility(self):
         return self.__facility
 
     @facility.setter
-    def facility(self, facility):
-        if not isinstance(facility, Facility):
-            raise TypeError("facility must be a Facility")
-        self.__facility = facility
+    def add_facility(self, facility: Facility):
+        self.__facility.append(facility)
 
+#for room maybe following would be better:
+# def add_room(self, room_id:int, number:str, price_per_night:float):
+# room = Room(room:id, number, price_per_night)
+# self.__room.append(room)
 
-
-
-
+    # def show_hotel_details(self):
+    #     print(f"Hotel Name: {self.__name}, Stars: {self.__stars}, Address: {self.__address}")
+    #     for room in self.__room:
+    #         print(room.get_room_details())
 
