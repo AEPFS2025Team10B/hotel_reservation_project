3de03bbe06d21b3e4cf2f9e269e31ca089af9296
david <david.germann@students.fhnw.ch>
2025-05-14 12:05:39 +0200

    in app.py added while loop, changed the texts for errors

--- app.py
+++ app.py
@@ -27,37 +27,45 @@ def main():
     # User Story 1.5: Kombination von Wünschen (Sterne, Gästezahl, Zeitraum).
     # User Story 1.6: Anzeige von Hoteldetails (Name, Adresse, Sterne).
 
-    stories = [
-        (1, "01.1_search_hotels_by_city_ui.py", "As a guest, I want to browse all hotels in a city so that I can choose one based on location (city)."),
-        (2, "01.2_search_hotel_stars.py", "As a guest, I want to browse all hotels in a city so that I can choose one based on location (city) and minimum star count."),
-        (3, "01.3_search_room_by_guest_count.py", "As a guest I want to search all hotels in a city that have room for all my guests."),
-        (4, "01.4_search_hotels_by_availability.py", "As a guest I want to search all hotels which have available room for my desired stay time."),
-        (5, "01.5_search_hotels_by_multiple_criteria.py", "As a guest I want to search hotels by multiple criteria (location, stars, guests, dates)."),
-        (6, "01.6_search_all_hotel_details.py", "As a guest I want to see hotel details (name, address, stars)."),
-        (7, "2.1_search_room_type_by_hotel.py", "As A guest, I want to see all Room Types of a Hotel")
-        ]
+    is_in_use = True
 
-    print("\nAvailable User Stories:")
-    for idx, fname, doc in stories:
-        print(f"{idx}. {fname}\n   → {doc}\n")
+    while is_in_use:
+        stories = [
+            (1, "01.1_search_hotels_by_city_ui.py", "As a guest, I want to browse all hotels in a city so that I can choose one based on location (city)."),
+            (2, "01.2_search_hotel_stars.py", "As a guest, I want to browse all hotels in a city so that I can choose one based on location (city) and minimum star count."),
+            (3, "01.3_search_room_by_guest_count.py", "As a guest I want to search all hotels in a city that have room for all my guests."),
+            (4, "01.4_search_hotels_by_availability.py", "As a guest I want to search all hotels which have available room for my desired stay time."),
+            (5, "01.5_search_hotels_by_multiple_criteria.py", "As a guest I want to search hotels by multiple criteria (location, stars, guests, dates)."),
+            (6, "01.6_search_all_hotel_details.py", "As a guest I want to see hotel details (name, address, stars)."),
+            (7, "2.1_search_room_type_by_hotel.py", "As A guest, I want to see all Room Types of a Hotel")
+            ]
 
-    try:
-        choice = int(input("Enter number to run or 0 to run all: "))
-    except ValueError:
-        print("Invalid input.")
-        return
+        print("\nAvailable User Stories:")
+        for idx, fname, doc in stories:
+            print(f"{idx}. {fname}\n   → {doc}\n")
 
-    if choice == 0:
-        for _, fname, _ in stories:
-            print(f"\n--- Running {fname} ---")
-            run_story(fname)
-    else:
-        match = next((f for i, f, _ in stories if i == choice), None)
-        if match:
-            print(f"\n--- Running {match} ---")
-            run_story(match)
+        try:
+            choice = int(input("Enter number you want to run (99 to exit): "))
+        except ValueError:
+            print("Invalid input pleas enter a nuber displayed.")
+            continue
+
+
+        if choice == 0:
+            for _, fname, _ in stories:
+                print(f"\n--- Running {fname} ---")
+                run_story(fname)
+
+        elif choice == 99:
+            is_in_use = False
+
         else:
-            print("Invalid choice.")
+            match = next((f for i, f, _ in stories if i == choice), None)
+            if match:
+                print(f"\n--- Running {match} ---")
+                run_story(match)
+            else:
+                print("Invalid choice, pleas enter a nuber displayed or 99 to exit")
 
 if __name__ == "__main__":
     main()
