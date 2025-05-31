c62ad55dddcf3c6650cafd3155999f32e256ee29
david <david.germann@students.fhnw.ch>
2025-05-31 21:25:36 +0200

    updated address_data_acces.py

--- data_access/address_data_acces.py
+++ data_access/address_data_acces.py
@@ -1,1 +1,38 @@
-#TODO: entire thing
+from data_access.base_data_access import BaseDataAccess
+from model.address import Address
+
+
+class AddressDataAccess(BaseDataAccess):
+    def __init__(self, db_path: str = None):
+        super().__init__(db_path)
+
+    def get_address_by_id(self, address_id: int) -> Address | None:
+        sql = """
+        SELECT address_id, street, city, zip_code
+        FROM address
+        WHERE address_id = ?
+        """
+        row = self.fetchone(sql, (address_id,))
+        if row:
+            return Address(*row)
+        return None
+
+    def insert_address(self, street: str, city: str, zip_code: str) -> int:
+        sql = """
+        INSERT INTO address (street, city, zip_code)
+        VALUES (?, ?, ?)
+        """
+        new_id, _ = self.execute(sql, (street, city, zip_code))
+        return new_id
+
+    def update_address(self, address: Address) -> None:
+        sql = """
+        UPDATE address
+        SET street = ?, city = ?, zip_code = ?
+        WHERE address_id = ?
+        """
+        self.execute(sql, (address.street, address.city, address.zip_code, address.address_id))
+
+    def delete_address(self, address_id: int) -> None:
+        sql = "DELETE FROM address WHERE address_id = ?"
+        self.execute(sql, (address_id,))
