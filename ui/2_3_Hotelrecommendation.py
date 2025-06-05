5697652c5fe38c9bb5b514886161afd6450c1540
david <david.germann@students.fhnw.ch>
2025-06-05 09:06:25 +0200

    lokale Änderungen vor Pull

--- ui/2_3_Hotelrecommendation.py
+++ ui/2_3_Hotelrecommendation.py
@@ -1,8 +1,26 @@
 """
 3. Als Gast möchte ich nach meinem Aufenthalt Hotelbewertungen abgeben
 """
-
+#Ziel ist es, dass der User die Buchungs ID eingeben muss, diese dann zuerst abgeglichen wir,
+# ob sie überhaupt existiert.
+# Danach kann der User seine Bewertung abgeben und es wird gespeichert.
 
 import sys
 import os
 from datetime import datetime
+
+
+from data_access.booking_data_access import BookingDataAccess
+from business_logic.booking_manager import find_booking_by_id, add_new_hotelrecommendation
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+def ask_booking_id():
+    valid = False
+    while not valid:
+        user_input = int(input("Please enter your booking id: "))
+        correct_booking_id = find_booking_by_id(user_input)
+
+
+ask_booking_id()
