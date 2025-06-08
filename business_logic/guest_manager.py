a11ac8caa80ad986258b275a530e16850a642a70
david <david.germann@students.fhnw.ch>
2025-06-08 16:17:13 +0200

    added get_guest_demographics in guest_manger.py

--- business_logic/guest_manager.py
+++ business_logic/guest_manager.py
@@ -1,12 +1,16 @@
 #Hier später Funktionen bzgl:
 #Gäste erstellen und lesen
 
-from data_access import GuestDataAccess
+from data_access import GuestDataAccess, BookingDataAccess
 from model.guest import Guest
+from datetime import datetime
+from common_code.age_calculator import calculate_age
+import collections
 
 
 # DAO-Instanzen
 guest_dao = GuestDataAccess()
+booking_dao = BookingDataAccess()
 
 def add_new_guest(first_name:str, last_name:str, email:str, street: str, city:str, zip:str, nationality: str, birthday) -> Guest:
     from business_logic.address_manager import find_address_id
@@ -18,3 +22,41 @@ def find_guest_by_email(email: str) -> Guest | None:
 
 def find_guest_by_id(guest_id:int):
     return guest_dao.get_guest_by_id(guest_id)
+
+def get_guest_demographics():
+    all_guests = guest_dao.get_all_guests()
+    all_bookings = booking_dao.get_all_bookings()
+
+    age_groups = collections.defaultdict(int)
+
+    nationalities = collections.defaultdict(int)
+
+    guest_booking_counts = collections.defaultdict(int)
+
+    for guest_obj in all_guests:
+        # das alter wird berechnet
+        if guest_obj.birthday:
+            try:
+                age = calculate_age(guest_obj.birthday)
+                if 0 <= age <= 25: age_groups["0-25"] += 1
+                elif 26 <= age <= 35: age_groups["26-35"] += 1
+                elif 36 <= age <= 45: age_groups["36-45"] += 1
+                elif 46 <= age <= 70: age_groups["46-70"] += 1
+                else: age_groups["70+"] += 1
+            except ValueError:
+                pass
+        
+        # hier werden die nationalitäten gezählt
+        if guest_obj.nationality:
+            nationalities[guest_obj.nationality] += 1
+
+    for booking in all_bookings:
+        guest_booking_counts[booking.guest_id] += 1
+
+    recurring_guests_count = sum(1 for count in guest_booking_counts.values() if count > 1)
+
+    return {
+        "age_groups": dict(age_groups),
+        "nationalities": dict(nationalities),
+        "recurring_guests": recurring_guests_count
+    }
